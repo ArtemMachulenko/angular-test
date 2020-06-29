26c2160717b7ec6ba1fde5a913f32a1af1e330fd
ArtemMachulenko <dr.a.mefesto@gmail.com>
2020-06-29 21:04:56 +0300

    -test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Hello</h1>
+    {{title}}
+  `,
+  styles: ['']
 })
 export class AppComponent {
   title = 'angular-test';
